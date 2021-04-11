8f0997791182213eba9ca49b359420eea6b5d222
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-11 15:36:52 +0200

    update the insert method

--- Tries.ts
+++ Tries.ts
@@ -1,12 +1,35 @@
 class Char {
-  char: string;
+  value: string;
   children: Char[];
   isEndOfWord: boolean;
-  constructor(char, isEndOfWord) {
-    this.char = char;
+  constructor(value) {
+    this.value = value;
     this.children = [];
-    this.isEndOfWord = isEndOfWord ? isEndOfWord : false;
   }
 }
 
+class Trie {
+  root: Char;
+  constructor() {
+    this.root = new Char(' ');
+  }
+  insert(word: string) {
+    let current = this.root;
+    const CHAR_CODE_OF_lETTER_A = 97;
+    for (let i = 0; i < word.length; i++) {
+      const char = word[i];
+      const idx = word.charCodeAt(i) - CHAR_CODE_OF_lETTER_A;
 
+      if (!current.children[idx]) {
+        current.children[idx] = new Char(char);
+      }
+      current = current.children[idx];
+    }
+    current.isEndOfWord = true;
+  }
+}
+
+var trie = new Trie();
+trie.insert('cat');
+trie.insert('can');
+console.log(trie);
